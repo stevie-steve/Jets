d417c3967c3e1307a1ccd6bdae1457a6e73bddd4
stevie-steve <sharpsrjr@gmail.com>
2020-04-06 08:05:11 -0600

    last minor update. COMPLETE

--- src/com/skilldistillery/jets/JetImpl.java
+++ src/com/skilldistillery/jets/JetImpl.java
@@ -5,7 +5,8 @@ public class JetImpl extends Jet {
 	@Override
 	public void fly() {
 		super.fly();
-		System.out.println("i fly at the speed i fly at");
+		System.out.println("I am a Passenger jet and I fly at the speed my passengers feel comfortable with.");
+		System.out.println();
 	}
 
 	public JetImpl(String type, String model, double speed, int range, double price) {
